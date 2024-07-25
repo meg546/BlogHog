7c5e8481bdcd004df03a5687f4b285a9b7ab74fb
ebs233@nau.edu <ebs233@nau.edu>
2024-07-24 18:50:11 -0700

    Base components

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+function App() {
+    return(
+        <div>
+        <Navbar />
+            <h1>
+                Hello World!
+            </h1>
+        </div>
+    )
+}
+
+
+
+export default App;
