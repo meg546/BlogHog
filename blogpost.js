d7c9fb90727a99989a53b26924938edfe701d9bb
ebs233@nau.edu <ebs233@nau.edu>
2024-07-27 20:23:49 -0700

    Catch up

---
+++ blogpost.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+
+const blogpostSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+
+    // This will be used to handle the routing for the URL
+    route: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    tags: {
+        type: [String],
+        default: []
+    },
+
+    // Used for blogposts that users are still working on but saved in DB
+    published: {
+        type: Boolean,
+        default: false
+    },
+
+    // Filepath will be stored in type
+    coverImage: {
+        type: String,
+        trim: true
+    },
+    comments: [{
+        user: {
+            type: String,
+            required: true
+        },
+        text: {
+            type: String,
+            required: true
+        },
+        date: {
+            type: Date
+        }
+    }],
+    likes: {
+        type: Number,
+        default: 0
+    }
+}, {
+    timestamps: true
+});
+
+const Blogpost = mongoose.model('Blogpost', blogpostSchema);
+
+module.exports = Blogpost;
